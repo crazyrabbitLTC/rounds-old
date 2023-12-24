91656698ee9988fead6acabe3c499856600d7f39
crazyrabbitLTC <dennison@dennisonbertram.com>
2023-12-24 12:17:45 -0500

    feat: add deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,12 @@
+import { RoundsBase } from "../typechain-types";
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = ethers.parseEther("0.001");
+  const roundsBase = await ethers.deployContract("RoundsBase");
+  await roundsBase.waitForDeployment();
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log(`RoundsBase deployed to ${roundsBase.target}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
